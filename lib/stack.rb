b6d0953064afedb950791ddee1c3dff7c9713eaf
Daniel Rice <danielrice5627@gmail.com>
2023-09-11 13:31:13 -0700

    commit tree 8da73e86e22391effcbbca86d7189195aa6c32b1

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,19 @@
-# your code here
+class Stack
+    def initialize
+        @stack = []
+    end
+
+    def push(token)
+        @stack.push(token)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[@stack.length-1]
+    end
+
+
+end
